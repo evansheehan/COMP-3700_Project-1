ddc836dbf08125cccb5bd32b5b880cd07c861e16
awubis <awubis@gmail.com>
2019-10-16 17:11:07 -0500

    Initial Commit

---
+++ src/TXTReceiptBuilder.java
@@ -0,0 +1,25 @@
+public class TXTReceiptBuilder implements IReceiptBuilder {
+
+    StringBuilder sb = new StringBuilder();
+
+    public void appendHeader(String header) {
+        sb.append(header).append("\n");
+    }
+
+    public void appendCustomer(CustomerModel customer) {
+        sb.append("Customer ID: ").append(customer.mCustomerID).append("\n");
+        sb.append("Customer Name: ").append(customer.mName).append("\n");
+    }
+
+    public void appendProduct(ProductModel product) {
+
+    }
+
+    public void appendPurchase(PurchaseModel purchase) {
+
+    }
+
+    public void appendFooter(String footer) {
+        sb.append(footer).append("\n");
+    }
+}
